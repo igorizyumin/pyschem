8ce6edfc2217c84791d2d153626d00d25bc4d5b6
Oleg Izyumin <oleg.izyumin@gmail.com>
2016-02-07 14:39:19 -0800

    Implemented closing tabs

--- sch/forms/main.py
+++ sch/forms/main.py
@@ -122,9 +122,7 @@ class MainWindow(QMainWindow):
 
     def installTab(self, newTab):
         # disconnect signals from old tab
-        if self.activeTab is not None:
-            self.activeTab.undoChanged.disconnect(self.onTabUndoChanged)
-            self.toolsDock.toolChanged.disconnect(self.activeTab.ctrl.changeTool)
+        self.disconnectActiveTab()
         if newTab is not None:
             # restore active tool
             self.toolsDock.on_toolChanged(newTab.ctrl.toolType)
@@ -135,6 +133,12 @@ class MainWindow(QMainWindow):
             self.onTabUndoChanged(newTab.canUndo(), newTab.canRedo())
         else:
             self.onTabUndoChanged(False, False)
+
+    def disconnectActiveTab(self):
+        if self.activeTab is not None:
+            self.activeTab.undoChanged.disconnect(self.onTabUndoChanged)
+            self.toolsDock.toolChanged.disconnect(self.activeTab.ctrl.changeTool)
+        self.activeTab = None
 
     @pyqtSlot(AbstractPage)
     def on_pageOpen(self, page):
@@ -155,6 +159,12 @@ class MainWindow(QMainWindow):
         if idx >= 0:
             tab = self.ui.tabWidget.widget(idx)
         self.installTab(tab)
+
+    @pyqtSlot(int)
+    def on_tabWidget_tabCloseRequested(self, idx):
+        if self.ui.tabWidget.currentIndex() == idx:
+            self.disconnectActiveTab()
+        self.ui.tabWidget.removeTab(idx)
 
 
 class PageTab(QWidget):
